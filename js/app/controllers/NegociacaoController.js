bff7d74622f3091f66c71a99e1602c420e49df14
gantognoli <gabriel_antognoli@hotmail.com>
2020-02-18 22:27:12 -0300

    Aula 4 completa

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -1,13 +1,33 @@
 class NegociacaoController {
     constructor(){
         let $ = document.querySelector.bind(document);
-        this.data = $('#data').value;
-        this.quantidade = $('#quantidade').value;
-        this.valor = $('#valor').value;
+        this._data = $('#data');
+        this._quantidade = $('#quantidade');
+        this._valor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adicionar(event){
         event.preventDefault();
-                
+
+        this._listaNegociacoes.adicionar(this._criaNegociacao());
+
+        this._limpaFormulario();
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._data.value), 
+            this._quantidade.value,
+            this._valor.value
+        );
+    }
+
+    _limpaFormulario(){
+        this._data.value = '';
+        this._quantidade = 1;
+        this._valor = 0.0;
+
+        this._data.focus();
     }
 }
