2850a9759e0c65414beb69a8af097353ab3651a9
gantognoli <gabriel_antognoli@hotmail.com>
2020-03-21 14:53:09 -0300

    Adicionando persistencia com IndexedDB

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -1,74 +1,80 @@
 class NegociacaoController {
-    constructor(){
+
+    constructor() {
         let $ = document.querySelector.bind(document);
         this._data = $('#data');
         this._quantidade = $('#quantidade');
         this._valor = $('#valor');
-        
+
         this._listaNegociacoes = new Bind(
             new ListaNegociacoes(), new NegociacoesView($('#negociacoesView')), 'adicionar', 'esvazia');
 
         this._mensagem = new Bind(new Mensagem, new MensagemView($('#mensagemView')), 'texto');
+
+        ConnectionFactory.getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .then(negociacoes =>
+                negociacoes.forEach(negociacao =>
+                    this._listaNegociacoes.adicionar(negociacao)))
+            .catch(erro => {
+                console.log(erro);
+                this._mensagem.texto = erro;
+            });
     }
 
-    adicionar(event){
+    adicionar(event) {
         event.preventDefault();
-        this._listaNegociacoes.adicionar(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação inserida com sucesso!';
-        this._limpaFormulario();
+
+        ConnectionFactory.getConnection()
+            .then(connection => {
+                let negociacao = this._criaNegociacao();
+                new NegociacaoDao(connection).adicionar(negociacao)
+                    .then(() => {
+                        this._listaNegociacoes.adicionar(negociacao);
+                        this._mensagem.texto = 'Negociação inserida com sucesso!';
+                        this._limpaFormulario();
+                    });
+            })
+            .catch(erro => this._mensagem.texto = erro);
     }
 
     importaNegociacoes() {
         let service = new NegociacaoService();
 
         service.obterNegociacoes()
-        .then(negociacoes => {
-           negociacoes
-                .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
-                .forEach(negociacao => this._listaNegociacoes.adicionar(negociacao));
-            this._mensagem.texto = 'Negociações importadas com sucesso!';
-        })
-        .catch(erro => this._mensagem.texto = erro);
-
-        /*
-        service.obterNegociacoesSemana()
-            .then(negociacoes => { negociacoes.forEach(negociacao => 
-                this._listaNegociacoes.adicionar(negociacao));
+            .then(negociacoes => 
+                negociacoes.filter(negociacao => 
+                    !this._listaNegociacoes.negociacoes.some(negociacaoExistente => 
+                        JSON.stringify(negociacaoExistente) == JSON.stringify(negociacao))))
+            .then(negociacoes => {
+                negociacoes
+                    .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+                    .forEach(negociacao => this._listaNegociacoes.adicionar(negociacao));
                 this._mensagem.texto = 'Negociações importadas com sucesso!';
             })
             .catch(erro => this._mensagem.texto = erro);
-
-        service.obterNegociacoesSemanaAnterior()
-            .then(negociacoes =>  { negociacoes.forEach(negociacao => 
-                this._listaNegociacoes.adicionar(negociacao)) 
-                this._mensagem.texto = 'Negociações da semana anterior obtidas com sucesso!'
-            })
-            .catch(erro => this._mensagem.texto = erro);
-
-        service.obterNegociacoesSemanaRetrasada()
-            .then(negociacoes => { negociacoes.forEach(negociacao => 
-                this._listaNegociacoes.adicionar(negociacao))
-                this._mensagem.texto = 'Negociações da semana retrasada obtidas com sucesso!'
-            })
-            .catch(erro => this._mensagem.texto = erro);
-
-        */
     }
 
-    apaga(){
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = 'Negociações removidas com sucesso!';
+    apaga() {
+        ConnectionFactory.getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(mensagem => {
+                this._mensagem.texto = mensagem;
+                this._listaNegociacoes.esvazia();
+            });
     }
 
-    _criaNegociacao(){
+    _criaNegociacao() {
         return new Negociacao(
-            DateHelper.textoParaData(this._data.value), 
-            this._quantidade.value,
-            this._valor.value
+            DateHelper.textoParaData(this._data.value),
+            parseInt(this._quantidade.value),
+            parseFloat(this._valor.value)
         );
     }
 
-    _limpaFormulario(){
+    _limpaFormulario() {
         this._data.value = '';
         this._quantidade.value = 1;
         this._valor.value = 0.0;
